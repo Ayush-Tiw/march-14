a741d76646c28b0fac0a7fa76fffa411bd6e8e17
Ayush-Tiw <99787142+Ayush-Tiw@users.noreply.github.com>
2022-07-19 22:18:54 +0530

    Add files via upload

---
+++ recap.js
@@ -0,0 +1,88 @@
+//-->comment in javascript
+//first js program
+console.log("Ayush Tiwari");
+
+// logic for even or odd
+// 1. input a number
+// 2.if a number is divisibe by 2 it is even else it is odd
+// 3.print my output
+
+
+//another way to display
+// document.write("i live in bangalore");//this will not display in console/terminal, throw an error
+
+//console.log display the output on the console of your browser/terminal and document.write display on browser,it does not display om terminal or console
+
+//it is loosely type language it does not define data type 
+
+//let is used to define variable
+// let number1=25;//integer data type
+
+// let number2="25"// string data type
+
+// const is define for constants
+// const PI=3.141;
+
+// const url="https://www.google.com";
+
+// const port=5001;
+
+// console.log(url+ ":"+port);//concatenation
+
+// "use strict";//kindly make the Js code compliant to JS-6 version only
+
+// let num1=5;
+// let num2=10;
+// console.log(num1+num2);
+
+
+//use js to find the number is even or odd number
+
+// "use strict";
+// let num1=5;
+
+// if(num1%2==0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+// let name="Ayush";
+
+// if(name=="Ayush"){
+//     console.log("student")
+// }
+// else if(name=="Manish"){
+//     console.log("teacher")
+// }
+// else if(name=="Abhishek"){
+//     console.log("FSD")
+// }
+// else{
+//     console.log("dont know")
+// }
+
+
+//for loop in js
+
+// "use strict";--
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// let count=0;
+// let user_name="suman";
+// for(let i=0;i<user_name.length;i++){
+//     count=count+1;
+// }
+// console.log(count);
+
+// //example of iteration over json data
+// let data={name:"Ayush",location:"bangalore"};
+
+// for(let key in data){
+//     console.log(key+":"+ data[key])
+// }
+
